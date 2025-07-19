48c82a2bc377acd54079183bcbfdaf067ccbe68d
Viola Hadjikyriacou <viky.vavrycukova@gmail.com>
2025-07-19 12:24:36 +0300

    Initialize uv python project and add numpy

---
+++ main.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def main():
+    print("Hello from time-series-analysis!")
+    print (np.__version__)
+    print(np.array([1, 2, 3, 4, 5]))
+    print("This is a simple time-series analysis example.")
+
+
+if __name__ == "__main__":
+    main()
